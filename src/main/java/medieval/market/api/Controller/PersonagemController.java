0a84de9b38a4eb1df907482bb27842f6e4e454ff
GusLopes14 <guslopes47@gmail.com>
2025-04-25 13:47:36 -0300

    last final update

--- src/main/java/medieval/market/api/Controller/PersonagemController.java
+++ src/main/java/medieval/market/api/Controller/PersonagemController.java
@@ -1,12 +1,9 @@
 package medieval.market.api.controller;
 
-import java.util.List;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.annotation.CacheEvict;
-import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort.Direction;
